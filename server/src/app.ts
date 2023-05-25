a34cebd6d5920a095b00b4e527beb24cbb8783ac
Daniel Figueroa <danfigueroa81@gmail.com>
2023-05-25 09:08:22 -0400

    add better error handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import notesRoutes from "./routes/notes"
+import createHttpError, { isHttpError } from "http-errors";
+
 // There was a type error here, sometimes imports in TS result in this, try the command given or make a d.ts file
 import morgan from "morgan"
 
@@ -16,7 +18,8 @@ app.use("/api/notes", notesRoutes)
 
 // For requests for routers in which we have no endpoint for
 app.use((req, res, next) => {
-    next(Error("Endpoint not found"));
+    // CreateHttpError needs the status code and the accompanying description
+    next(createHttpError(404, "Endpoint not found"));
 });
 
 // Error handlers are a form of middleware of their own
@@ -25,8 +28,16 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     // Code block to catch error and pass it accordingly that every endpoint should have for safety
     console.error(error);
     let errorMessage = "An unknown error occurred!";
-    if (error instanceof Error) errorMessage = error.message;
+    let statusCode = 500;
+    // Check if the error is a type of HttpError
+    if (isHttpError(error)) {
+        // If so, get that determined status and message, then return as a JSON object
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
+    // The only case it should get through without going in the body of the if statement would be if the
+    // error is truly enigmatic
     // use {} to define JSON
-    res.status(500).json({error: errorMessage})
+    res.status(statusCode).json({ error: errorMessage });
 });
 export default app;
--- server/src/controllers/notes.ts
+++ server/src/controllers/notes.ts
@@ -1,5 +1,7 @@
 import NoteModel from "../models/note"
+import mongoose from "mongoose";
 import { RequestHandler } from "express";
+import createHttpError from "http-errors";
 
 // export const is used to export multiple things 
 // When this was initially moved here, req, res and next produced errors as they don't have a type
@@ -21,17 +23,42 @@ export const getNote: RequestHandler = async (req, res, next) => {
     const noteId = req.params.noteId;
 
     try {
+        // Check if the given noteId adheres to the structures of ids
+        if (!mongoose.isValidObjectId(noteId)) {
+            throw createHttpError(400, "Invalid note id");
+        }
+
         const note = await NoteModel.findById(noteId).exec();
+
+        // Provided a valid id, check if the note fetched is null/does not exist
+        if (!note) {
+            throw createHttpError(404, "Note not found");
+        }
+
         res.status(200).json(note);
     } catch (error) {
         next(error);
     }
 };
 
-export const createNote: RequestHandler = async (req, res, next) => {
+// These are akin to types, acting like objects to make req.body.title less ambiguous
+// Prior to this, the error handling here was not optimal and even missing/entering the title
+// incorrectly would lead to errors
+interface CreateNoteBody {
+    title?: string,
+    text?: string,
+}
+
+// We pass unknown instead of any because the latter is too unrestricted and ambiguous. 
+// unknown has a greater degree of restriction
+export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknown> = async (req, res, next) => {
     const title = req.body.title;
     const text = req.body.text;
     try {
+        // Ensure the title is there for the notes
+        if (!title) {
+            throw createHttpError(400, "Note must have a title");
+        }
         // provided the title and text, we create a new NoteModel object
         const newNote = await NoteModel.create({
             title: title,
