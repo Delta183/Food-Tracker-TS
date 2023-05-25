fc016c6e2afa4b6c50f87f3be0cba95ccbccf9dc
Daniel Figueroa <danfigueroa81@gmail.com>
2023-05-25 10:49:51 -0400

    import bootstrap, modify app.tsx

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'react-bootstrap';
 
 function App() {
+  // React needs a special type of variable for updated value
+  const [clickCount, setClickCount] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Subscribe for more MIX
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* This is how anonymous functions are passed */}
+       <Button onClick={() => setClickCount(clickCount+1)}>
+        {/* using the squiggly brackets to use the react variables */}
+        Clicked {clickCount} times
+       </Button>
+        
       </header>
     </div>
   );
