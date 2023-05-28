44609c8d22b1cf2cfe3db3feb0be0a425b57cd9b
Daniel Figueroa <danfigueroa81@gmail.com>
2023-05-28 13:41:23 -0400

    user authentication with cookies/sessions

---
+++ server/src/controllers/users.ts
@@ -0,0 +1,120 @@
+import { RequestHandler } from "express";
+import createHttpError from "http-errors";
+import UserModel from "../models/user";
+import bcrypt from "bcrypt";
+
+// This is how we get the information from the database if the user is logged in,
+export const getAuthenticatedUser: RequestHandler = async (req, res, next) => {
+    const authenticatedUserId = req.session.userId;
+
+    try {
+        if (!authenticatedUserId) {
+            throw createHttpError(401, "User not authenticated");
+        }
+
+        const user = await UserModel.findById(authenticatedUserId).select("+email").exec();
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
+
+// a class to facilitate a streamlined consumption of data
+interface SignUpBody {
+    username?: string,
+    email?: string,
+    password?: string,
+}
+
+export const signUp: RequestHandler<unknown, unknown, SignUpBody, unknown> = async (req, res, next) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const passwordRaw = req.body.password; // It is raw as this is the unhashed version
+    // naming it as such makes it very evident this should not be the value saved to the database
+
+    try {
+        // Input validation  
+        if (!username || !email || !passwordRaw) {
+            throw createHttpError(400, "Parameters missing");
+        }
+
+        const existingUsername = await UserModel.findOne({ username: username }).exec();
+
+        // Check if username is taken
+        if (existingUsername) {
+            throw createHttpError(409, "Username already taken. Please choose a different one or log in instead.");
+        }
+
+        const existingEmail = await UserModel.findOne({ email: email }).exec();
+
+        // Check if email has already been used for this account
+        if (existingEmail) {
+            throw createHttpError(409, "A user with this email address already exists. Please log in instead.");
+        }
+
+        const passwordHashed = await bcrypt.hash(passwordRaw, 10);
+
+        const newUser = await UserModel.create({
+            username: username,
+            email: email,
+            password: passwordHashed,
+        });
+
+        // This issue of userId not already knowing what type is needed was subverted with the @types folder
+        req.session.userId = newUser._id;
+
+        res.status(201).json(newUser);
+    } catch (error) {
+        next(error);
+    }
+};
+
+// Similar to the signup but for login information
+interface LoginBody {
+    username?: string,
+    password?: string,
+}
+
+export const login: RequestHandler<unknown, unknown, LoginBody, unknown> = async (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        // No username or password
+        if (!username || !password) {
+            throw createHttpError(400, "Parameters missing");
+        }
+
+        // The +password and +email are explicit requests for the values from the database
+        const user = await UserModel.findOne({ username: username }).select("+password +email").exec();
+
+        if (!user) {
+            // For user and password give the same error message so the user doesn't brute force
+            // their way into other accounts
+            throw createHttpError(401, "Invalid credentials");
+        }
+
+        // Check if the passwords match
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw createHttpError(401, "Invalid credentials");
+        }
+
+        req.session.userId = user._id;
+        res.status(201).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
+
+// Upon a logout, destroy the session that was underway
+export const logout: RequestHandler = (req, res, next) => {
+    req.session.destroy(error => {
+        if (error) {
+            next(error);
+        } else {
+            res.sendStatus(200);
+        }
+    });
+};
