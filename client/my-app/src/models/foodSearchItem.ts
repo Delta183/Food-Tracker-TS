572d77df8242457d5355e63eac0c2b15d09eae64
Daniel Figueroa <danfigueroa81@gmail.com>
2023-06-01 13:21:38 -0400

    revise model to hold nested json response

--- client/my-app/src/models/foodSearchItem.ts
+++ client/my-app/src/models/foodSearchItem.ts
@@ -1,12 +1,13 @@
 export interface foodSearchItem {
-  food_name: string;
-  serving_qty: number;
-  tag_id: string;
-  photo: PhotoArray; // This is simply because of the way the api returned this information
-}
+    food_name: string;
+    serving_qty: number;
+    tag_id: string;
+    photo: PhotoArray; // This is simply because of the way the api returned this information did so in an array
+  }
 
-export interface PhotoArray {
-  thumb: string;
-}
+  // This is how we handle nested arrays according
+  export interface PhotoArray{
+    thumb: string;
+  }
 
 // The search query and the nutrition query provide different results and thus must separate the food models
